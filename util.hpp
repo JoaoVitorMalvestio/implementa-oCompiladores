80cbbdf30e3f2c9be45d1a803d9fb2a0137dd236
Michel Bolzon <michel.bolzon123@gmail.com>
2021-05-04 19:53:39 -0300

    Convertido util.cpp e util.h e adicionado alguns arquivos no .gitignore

--- util.hpp
+++ util.hpp
@@ -1,13 +1,9 @@
 #include <assert.h>
 
 typedef char *string;
-typedef char bool;
-
-#define TRUE 1
-#define FALSE 0
 
 void *checked_malloc(int);
-string String(char *);
+//string String(char *);
 
 typedef struct U_boolList_ *U_boolList;
 struct U_boolList_ {bool head; U_boolList tail;};
