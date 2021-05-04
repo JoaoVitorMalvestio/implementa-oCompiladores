4ba842c7939c2ffe2db25fa2cdf5ad5073a978d9
Michel Bolzon <michel.bolzon123@gmail.com>
2021-05-04 20:05:49 -0300

    Alterado printf para cout

--- parse.cpp
+++ parse.cpp
@@ -8,6 +8,7 @@
 #include "absyn.h"
 #include "errormsg.h"
 #include "parse.hpp"
+#include <iostream>
 #include <cstdlib>
 #include "semant.c"
 
@@ -29,8 +30,11 @@ A_exp parse(string fname) {
 }
 
 int main(int argc, char **argv) {
- if (argc!=2) {fprintf(stderr,"usage: a.out filename\n"); exit(1);}
- A_exp ast = parse(argv[1]);
- SEM_transProg(ast);
- return 0;
+ if (argc!=2) {
+    cout << "usage: a.out filename\n";
+    exit(1);
+  }
+  A_exp ast = parse(argv[1]);
+  SEM_transProg(ast);
+  return 0;
 }
