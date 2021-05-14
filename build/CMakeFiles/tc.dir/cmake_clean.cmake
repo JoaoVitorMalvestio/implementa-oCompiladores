2472ac875485ff86f83614a1c49948892028b578
Michel Bolzon <michel.bolzon123@gmail.com>
2021-05-14 19:34:22 -0300

    Ajustados os ignore

---
+++ build/CMakeFiles/tc.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "parser.cc"
+  "parser.hh"
+  "scanner.cc"
+  "CMakeFiles/tc.dir/absyn.cc.o"
+  "CMakeFiles/tc.dir/driver.cc.o"
+  "CMakeFiles/tc.dir/env.cc.o"
+  "CMakeFiles/tc.dir/errormsg.cc.o"
+  "CMakeFiles/tc.dir/main.cc.o"
+  "CMakeFiles/tc.dir/parse.cc.o"
+  "CMakeFiles/tc.dir/parsetest.cc.o"
+  "CMakeFiles/tc.dir/prabsyn.cc.o"
+  "CMakeFiles/tc.dir/semant.cc.o"
+  "CMakeFiles/tc.dir/symbol.cc.o"
+  "CMakeFiles/tc.dir/table.cc.o"
+  "CMakeFiles/tc.dir/types.cc.o"
+  "CMakeFiles/tc.dir/util.cc.o"
+  "CMakeFiles/tc.dir/scanner.cc.o"
+  "CMakeFiles/tc.dir/parser.cc.o"
+  "../tc.pdb"
+  "../tc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
